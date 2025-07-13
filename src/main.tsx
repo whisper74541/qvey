f475b68d57dbf0b731aefe0a548f4ea3eecd332c
ptk5758 <ptk725739@gmail.com>
2025-07-13 20:00:08 +0900

    [design] thema color

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './css/root.css'
 import './css/reset.css'
 import './css/global.css'
 import App from './App.tsx'
