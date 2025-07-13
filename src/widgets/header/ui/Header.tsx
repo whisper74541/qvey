6db297c634bfdac530c2ef669bf2b005faca4832
ptk5758 <ptk725739@gmail.com>
2025-07-14 00:39:42 +0900

    [Design] Button 기본 디자인

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,4 +1,20 @@
+import style from './Header.module.css'
 function Header() {
-    return <header>Head..</header>
+    return (
+        <header className={style.header}>
+            <div className={style.left}>
+                <h2>Header Text... Highway Star</h2>
+            </div>
+            <div className={style.right}>
+                <button>Button 1</button>
+            </div>
+            <div className={style.right}>
+                <button>Button 2</button>
+            </div>
+            <div className={style.right}>
+                <button className="outline">회원가입</button>
+            </div>
+        </header>
+    )
 }
 export default Header
