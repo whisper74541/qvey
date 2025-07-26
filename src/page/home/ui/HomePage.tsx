41211f0f6898653b08d123157db559c9ddf5a94d
ptk5758 <ptk725739@gmail.com>
2025-07-26 18:25:30 +0900

    [Design] Home Router 추가및 Style 수정

--- src/page/home/ui/HomePage.tsx
+++ src/page/home/ui/HomePage.tsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router'
 import style from './HomePage.module.css'
 function HomePage() {
     return (
         <div className={style.container}>
             <h2>Home Page..</h2>
+            <h3>바로가기</h3>
+            <Link to={'/book'}>
+                <b>문제집</b>
+            </Link>
+            <Link to={'/board'}>
+                <b>자유게시판</b>
+            </Link>
         </div>
     )
 }
