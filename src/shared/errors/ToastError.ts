01fb769a033418ee975039e7a2e56fc52b60797e
Daram0o0 <c30728535@gmail.com>
2025-07-27 15:49:38 +0900

    [Chore] barrel 파일 추가

--- src/shared/errors/ToastError.ts
+++ src/shared/errors/ToastError.ts
@@ -1,6 +1,6 @@
 import type { ToastType } from '@/widgets/toast/types'
 
-class ToastError extends Error {
+export class ToastError extends Error {
     toastType: ToastType
 
     constructor(message: string, toastType: ToastType) {
