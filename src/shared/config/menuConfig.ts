3d6f6a1797bd5f3c5a190ff23eb3280821e7607d
ptk5758 <ptk725739@gmail.com>
2025-08-02 14:19:41 +0900

    [DEMO] ToastPopup DEMO 추가

--- src/shared/config/menuConfig.ts
+++ src/shared/config/menuConfig.ts
@@ -4,6 +4,7 @@ const MENU_DATA: Menu[] = [
     { name: '문제집', path: '/book' },
     { name: '자유게시판', path: '/board' },
     { name: '개발자', path: '/dev' },
+    { name: 'DEMO Toast', path: '/demo/toast' },
 ]
 
 export default { menus: MENU_DATA }
